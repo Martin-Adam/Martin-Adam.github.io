65c547c83036b37bbe6abf78e1795e567f1c8da8
Martin-Adam <copperdog08@gmail.com>
2017-06-07 10:55:35 -0600

    Updated Dice

---
+++ D&D/Content/Script/dice.js
@@ -0,0 +1,18 @@
+function roll() {
+    var num = $("input[name=num]:checked").val();
+    var die = $("input[name=dice]:checked").val();
+    if (num == undefined || die == undefined) {
+        $("#Numbers").html("<p style='color: red'>Please select both options</p>");
+    } else {
+        var arraylist = [];
+        for (var i = num; i > 0; i--) {
+            arraylist.push(Math.floor(Math.random() * die) + 1);
+        }
+        var output = "";
+        for (var i = 0; i < arraylist.length - 1; i++) {
+            output += arraylist[i] + ", ";
+        }
+        output += arraylist[arraylist.length - 1];
+        $("#Numbers").html("<h2>" + output + "</h2>");
+    }
+}
--- D&D/Content/Scripts/dice.js
+++
@@ -1,9 +0,0 @@
-function roll(){
-  var num = $( "#number option:selected" ).text();
-  var output = [];
-  var die = $( "#diceamount option:selected" ).text();
-  for(var i = num; i > 0; i--){
-    output.push(Math.floor(Math.random() * die) + 1);
-  }
-  $("#Numbers").html(output.toString());
-}
